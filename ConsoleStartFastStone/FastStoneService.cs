0a18ef323aad90f08060b16247e3599d61c1bfa1
Dev65432 <i122123@yandex.ru>
2024-07-09 12:06:40 +0300

    Добавьте файлы проекта.

---
+++ ConsoleStartFastStone/FastStoneService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleStartFastStone
+{
+    static class FastStoneService
+    {
+        static string PathToFastStoneExe = @"c:\ProgramFilesPortable\FastStone_Image_Viewer_7.8_2023-09-28\FSViewer.exe";
+
+        static public async Task OpenInFastStoneExecuted(string fullNamePicture)
+        {
+            try
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = PathToFastStoneExe,
+                    Arguments = $"\"{fullNamePicture}\"",
+                    UseShellExecute = false
+                });
+
+            }
+            catch (Exception ex)
+            {
+                string msg = ex.Message;
+                throw;
+            }
+        }
+
+        static public async Task OpenInFastStoneExecuted2(string fullNamePicture)
+        {
+            try
+            {   
+                await Task.Run(() =>
+                {
+                    Process.Start(new ProcessStartInfo
+                    {
+                        FileName = PathToFastStoneExe,
+                        Arguments = $"\"{fullNamePicture}\"",
+                        UseShellExecute = false
+                    });
+                });
+
+            }
+            catch (Exception ex)
+            {
+                string msg = ex.Message;
+                throw;
+            }
+        }
+    }
+}
---
+++ ConsoleStartFastStone/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleStartFastStone
+{
+    class Program
+    {
+        // FastStoneService _fastStoneService;
+
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+
+            Test1();
+        }
+
+
+        static void Test1()
+        {
+            /*
+             * f:\SHARE2\Projects\20240709104824\01_pr\01\ConsoleStartFastStone\ConsoleStartFastStone\AppFiles\Imgs\Screenshot_1.png
+             * f:\SHARE2\Projects\20240709104824\01_pr\01\ConsoleStartFastStone\ConsoleStartFastStone\AppFiles\Imgs\Screenshot_8.png
+             */
+
+            string _directoryImgs = @"AppFiles\Imgs\";
+            string nameFile = "Screenshot_1.png";
+
+            var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, _directoryImgs, nameFile); // \AppFiles\Imgs\Screenshot_1.png
+
+            FastStoneService.OpenInFastStoneExecuted(filePath);
+        }
+    }
+}
